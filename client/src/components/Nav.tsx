20628c36a7f7010e058da961d3677e8c11595a44
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-04-05 07:55:11 +0100

    feat: highlighting in header navbar.

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,28 +1,36 @@
 import { NavLink } from "react-router-dom";
 import '../App.css';
 
-const Nav: React.FC = () =>
-    <>
-        <nav>
-            <ul className="NavUlFlex">
-                <li>
-                    <ul className="NavInnerULFlex">
-                        <li>FAKELANDIA</li>
-                        <li>JUSTICE</li>
-                        <li>DEPARTMENT</li>
-                    </ul>
-                </li>
-                <li>
-                    <NavLink to="/home">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/misdemeanour">Misdemeanours</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/confession">Confess to Us</NavLink>
-                </li>
-            </ul>
-        </nav>
-    </>
+
+const pageLinkTable = new Map([['home', 'Home'],
+['misdemeanour', 'Misdemeanours'],
+['confession', 'Confess to Us']]);
+
+const linkedPages: Array<string> = ["home", "misdemeanour", "confession"];
+
+const Nav: React.FC = () => {
+
+
+    const links: Array<React.ReactNode> = linkedPages.map((page) =>
+        <>
+            <li>
+                <NavLink to={'/' + page}
+                    style={({ isActive }) =>
+                        isActive ? { color: 'darkmagenta' } :
+                            { color: 'slategray' }}>
+                    {pageLinkTable.get(page)}</NavLink>
+            </li>
+        </>);
+
+    return (
+        <>
+            <nav>
+                <ul className="NavUlFlex">
+                    {...links}
+                </ul>
+            </nav>
+        </>
+    )
+}
 
 export default Nav;
